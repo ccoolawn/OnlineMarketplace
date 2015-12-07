e6a08e5c9bbbbc7cadcf507bac071704f7d32848
ccoolawn <cornell1001@mac.com>
2015-12-07 13:07:29 -0800

    Users can now place orders

---
+++ app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ActiveRecord::Base
+	validates :address, :city, :state, :zipcode, presence: true
+	validates :zipcode, zipcode: { country_code: :en }
+
+	belongs_to :listing
+	belongs_to :buyer, class_name: "User"
+	belongs_to :seller, class_name: "User"
+end
