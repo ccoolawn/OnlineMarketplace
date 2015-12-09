0819d3f1ecd15df92bc02b4ddf1da93f95198f2f
ccoolawn <cornell1001@mac.com>
2015-12-08 20:36:22 -0800

    Completed Stripe Integration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207193655) do
+ActiveRecord::Schema.define(version: 20151209024434) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20151207193655) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
