e8982cd6cdea0deb1131720528c52383c7a897f0
ccoolawn <cornell1001@mac.com>
2015-12-14 16:57:08 -0800

    Edited controller due to error via stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,26 +31,27 @@ class OrdersController < ApplicationController
 		@order.seller_id = @seller.id
 
 		require "stripe"
-
 		Stripe.api_key = ENV["STRIPE_API_KEY"]
 		token = params[:stripeToken]
 
 		begin
 			charge = Stripe::Charge.create(
-				:amount =>(@listing.price*100).floor,
+				:amount => (@listing.price * 100).floor,
 				:currency => "usd",
 				:card => token
-				)
+			)
+
 			flash[:notice] = "Thanks for ordering!"
-		rescue Stripe::CardError => e
-			flash[:danger] = e.message
+
+			rescue Stripe::CardError => e
+				flash[:danger] = e.message
 		end
 
 		transfer = Stripe::Transfer.create(
 			:amount => (@listing.price * 95).floor,
 			:currency => "usd",
 			:recipient => @seller.recipient
-			)
+		)
 
 		respond_to do |format|
 			if @order.save
